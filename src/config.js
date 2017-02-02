154e330975579b7c37c0961425ab5f38dbab2f57
William Hurley <wjhurley@comcast.net>
2017-02-02 06:32:44 +0000

    deploys twitter bot

--- src/config.js
+++ src/config.js
@@ -15,10 +15,10 @@
 // };
 
 module.exports = {
-  consumer_key: '',
-  consumer_secret: '',
-  access_token: '',
-  access_token_secret: '',
+  consumer_key: 'Jyq3xgYOVx0nTIP1a7nH6Db7l',
+  consumer_secret: 'B1sjrfByaFcNeguHrPqIwZco7hfV6Nz2wJwWVDuVkRuoyq4vhV',
+  access_token: '3146803586-wV7MeabWTWhP9vmAk0b5YXidjdMefAQitbnvQUC',
+  access_token_secret: 'r0Zneus1ss6ZA4qWqoRZYT8ofUZyc9M4kwJmlK44wXcS2',
 };
 
 
