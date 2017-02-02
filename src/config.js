6ddc4f1712c3d895b356bf6c2e35c7de26f32fec
William Hurley <wjhurley@comcast.net>
2017-02-02 06:50:23 +0000

    adds environment variables

--- src/config.js
+++ src/config.js
@@ -7,18 +7,9 @@
 
 // Uncomment for when deploying to Heroku
 
-// module.exports = {
-//   consumer_key: process.env.CONSUMER_KEY,
-//   consumer_secret: process.env.CONSUMER_SECRET,
-//   access_token: process.env.ACCESS_TOKEN,
-//   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-// };
-
 module.exports = {
-  consumer_key: 'Jyq3xgYOVx0nTIP1a7nH6Db7l',
-  consumer_secret: 'B1sjrfByaFcNeguHrPqIwZco7hfV6Nz2wJwWVDuVkRuoyq4vhV',
-  access_token: '3146803586-wV7MeabWTWhP9vmAk0b5YXidjdMefAQitbnvQUC',
-  access_token_secret: 'r0Zneus1ss6ZA4qWqoRZYT8ofUZyc9M4kwJmlK44wXcS2',
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 };
-
-
